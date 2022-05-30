dafc2b5b9a9f4442192ae259b27b7b9d8f449e3e
ElenaPonomareva <ponomareva@internet.ru>
2022-05-30 18:48:39 +0500

    reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,7 @@
 public class BonusMilesService {
     public int calculate(int priceTicket) {
         int bonus = 20;
-        int miles = priceTicket/bonus;
-        return miles;
+        return priceTicket / bonus;
     }
 
 }
